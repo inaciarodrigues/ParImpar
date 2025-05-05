c7eac5d38d50e838b135e6a02b8a3d95d396176d
Inácia Rodrigues <soterinacia688@gmail.com>
2025-05-05 11:58:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+string paridade;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+paridade = (numero % 2) == 0 ? "par" : "ímpar";
+
+Console.WriteLine($"{numero} é {paridade}");
